ce5f39bf940decedea0a2bb7c2df467a1161bb70
Hafiz Abeer <hafizabeer997outlook@gmail.com>
2026-02-17 05:04:26 +0500

    Deployment issues resolved

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://donor-hub-gold.vercel.app',
         changeOrigin: true,
       },
     },
