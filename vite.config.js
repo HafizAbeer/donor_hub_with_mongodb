ca258eb9b77055fcc4c4d59da1e0dbd8278874c7
Hafiz Abeer <hafizabeer997outlook@gmail.com>
2026-02-17 18:24:26 +0500

    favicon size adjusted

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'robots.txt', 'apple-touch-icon.png'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.png", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: 'Donor Hub',
-        short_name: 'DonorHub',
-        description: 'Blood Donation Management System',
-        theme_color: '#ffffff',
+        name: "Donor Hub",
+        short_name: "DonorHub",
+        description: "Blood Donation Management System",
+        theme_color: "#ffffff",
         icons: [
+          // {
+          //   src: 'favicon.png',
+          //   sizes: '192x192',
+          //   type: 'image/png'
+          // },
           {
-            src: 'favicon.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "favicon.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'favicon.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
           },
-          {
-            src: 'favicon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://donor-hub-gold.vercel.app',
+      "/api": {
+        target: "https://donor-hub-gold.vercel.app",
         changeOrigin: true,
       },
     },
   },
-})
+});
