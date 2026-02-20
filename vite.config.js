cff818e4c45a5e0473194002209c011ed6431840
Hafiz Abeer <hafizabeer997outlook@gmail.com>
2026-02-21 01:16:19 +0500

    favicon size adjusted

--- vite.config.js
+++ vite.config.js
@@ -17,18 +17,13 @@ export default defineConfig({
         description: "Blood Donation Management System",
         theme_color: "#ffffff",
         icons: [
-          // {
-          //   src: 'favicon.png',
-          //   sizes: '192x192',
-          //   type: 'image/png'
-          // },
           {
-            src: "favicon.png",
-            sizes: "512x512",
+            src: "/icon-192.png",
+            sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "favicon.png",
+            src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
